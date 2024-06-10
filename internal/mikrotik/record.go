1668eb877e050a7339242ea54c7def4e0640a00e
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-06-10 19:36:42 +0000

    cleanup

--- internal/mikrotik/record.go
+++ internal/mikrotik/record.go
@@ -1,27 +1,19 @@
 package mikrotik
 
 import (
-	"encoding/json"
 	"fmt"
 
-	log "github.com/sirupsen/logrus"
 	"sigs.k8s.io/external-dns/endpoint"
 )
 
 // NewDNSRecord converts an ExternalDNS Endpoint to a Mikrotik DNSRecord
 func NewRecordFromEndpoint(endpoint *endpoint.Endpoint) (*DNSRecord, error) {
-	jsonBody, err := json.Marshal(endpoint)
-	if err != nil {
-		log.Errorf("Error marshalling endpoint: %v", err)
-		return nil, err
-	}
-	log.Debugf("Endpoint to parse: %s", string(jsonBody))
-
 	record := DNSRecord{
 		Name:    endpoint.DNSName,
 		Type:    endpoint.RecordType,
 		Comment: "Managed by ExternalDNS",
 	}
+
 	switch endpoint.RecordType {
 	case "A", "AAAA":
 		record.Address = endpoint.Targets[0]
@@ -34,30 +26,13 @@ func NewRecordFromEndpoint(endpoint *endpoint.Endpoint) (*DNSRecord, error) {
 		return nil, fmt.Errorf("unsupported DNS type: %s", endpoint.RecordType)
 	}
 
-	jsonBody, err = json.Marshal(record)
-	if err != nil {
-		log.Errorf("Error marshalling dns record: %v", err)
-		return nil, err
-	}
-	log.Debugf("Record parsed: %s", string(jsonBody))
-
 	return &record, nil
 }
 
 func NewEndpointFromRecord(record DNSRecord) (*endpoint.Endpoint, error) {
-	jsonBody, err := json.Marshal(record)
-	if err != nil {
-		log.Errorf("Error marshalling record: %v", err)
-		return nil, err
-	}
-	log.Debugf("Record to parse: %s", string(jsonBody))
-
 	var ep endpoint.Endpoint
-	if record.Type == "" {
-		record.Type = "A"
-	}
 	switch record.Type {
-	case "A", "AAAA":
+	case "", "A", "AAAA": // "" means A record because mikrotik is weird like that... :P
 		ep = endpoint.Endpoint{
 			DNSName:    record.Name,
 			RecordType: record.Type,
