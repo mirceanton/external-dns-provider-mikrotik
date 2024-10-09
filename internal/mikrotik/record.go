7a68266cf2bc1da90200d247fbee59fb0348820d
Mircea-Pavel Anton <contact@mirceanton.com>
2024-10-09 21:14:30 +0300

    feat: Add support for A, AAAA, CNAME and TXT Record Types (#14)
    * add todo comment
    
    * add all fields from the mikrotik docs site to the dns record struct
    
    * test: print a json of the received object to see the structure
    
    * that didnt work out,let's try something else
    
    * ignore the deploy dir for now
    
    * slight refactor + implement all DNS types ???
    
    * fix bool parsing
    
    * use a non-stupid string to int conversion
    
    * set disable to be of type string since we don't really care
    
    * change all fields to be of type string since we dont care
    
    * convert EVERYTHING to string?
    
    * log debug the json body of the create request
    
    * fix
    
    * fix 2
    
    * try fix
    
    * log endpoint and record objects
    
    * implement conversion functions?
    
    * try fix?
    
    * idk...
    
    * dont omitempty on type?
    
    * this is getting silly
    
    * remove defaults as they apparently do nothing
    
    * cleanup
    
    * rename file
    
    * Rename functions
    
    * try some logging stuff
    
    * idek
    
    * chore: add support for github cli in devcontainer
    
    * chore: remove actionlint from devcontainer
    
    * chore: cleanup devcontainer
    
    * add tests for DNSRecord
    
    * fix typo
    
    * add test for dnsrecord from endpoint conversion
    
    * add test for endpoint from dnsrecord conversion
    
    * chore: Update GitHub Actions workflows to run on PRs targeting any branch
    
    * feat: assume A type for dns records if no type is set (#61)
    
    * chore: add copilot vscode extension
    
    * feat: Add DefaultDNSRecordValues struct for default DNS record values
    
    * chore: add sample values.yaml file for helm deployment
    
    * chore: update taskfile
    
    * chore: update test deployment values
    
    * chore: add example a-record DNSEndpoint for testing
    
    * chore: add example quad-a-record DNSEndpoint for testing
    
    * print providerspecific stuff
    
    * go mod tidy
    
    * chore: update taskfile
    
    * chore: update devcontainer configuration
    
    * chore: Refactor DNSRecord
    
    * fix tests
    
    * fix lint?
    
    * print banner using log so it's also json
    
    * ROS assume disabled: false if not set
    
    * ROS assume record type A if not set
    
    * fix
    
    * ignore logs
    
    * try fix and update tests
    
    * update test deploy records
    
    * update test deployment values
    
    * try logging possible error
    
    * cleanup plan changes before applying
    
    * ignore disabled field as it creates complications apparently
    
    * update test deploy stuff
    
    * remove default ttl and default comment functionality
    
    * update test deploy
    
    * update test deploy
    
    * update taskfile
    
    ---------
    
    Co-authored-by: Andrei Costescu <andrei@costescu.no>

---
+++ internal/mikrotik/record.go
@@ -0,0 +1,215 @@
+package mikrotik
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	"sigs.k8s.io/external-dns/endpoint"
+)
+
+// DNSRecord represents a MikroTik DNS record in the format used by the API
+// https://help.mikrotik.com/docs/display/ROS/DNS#DNS-DNSStatic
+type DNSRecord struct {
+	// Common fields for all record types
+	ID             string `json:".id,omitempty"`             // only fetched from API
+	Name           string `json:"name"`                      // endpoint.DNSName
+	Type           string `json:"type"`                      // endpoint.RecordType
+	TTL            string `json:"ttl,omitempty"`             // endpoint.RecordTTL
+	Comment        string `json:"comment,omitempty"`         // provider-specific
+	Regexp         string `json:"regexp,omitempty"`          // provider-specific
+	MatchSubdomain string `json:"match-subdomain,omitempty"` // provider-specific
+	AddressList    string `json:"address-list,omitempty"`    // provider-specific
+
+	// Disabled       string `json:"disabled,omitempty"`        // provider-specific
+
+	// Record specific fields
+	Address string `json:"address,omitempty"` // A, AAAA -> endpoint.Targets[0]
+	CName   string `json:"cname,omitempty"`   // CNAME -> endpoint.Targets[0]
+	Text    string `json:"text,omitempty"`    // TXT -> endpoint.Targets[0]
+
+	// Additional fields for other record types that are not currently supported
+	// MXExchange   string `json:"mx-exchange,omitempty"`   // MX -> provider-specific
+	// MXPreference string `json:"mx-preference,omitempty"` // MX -> provider-specific
+	// SrvPort      string `json:"srv-port,omitempty"`      // SRV -> provider-specific
+	// SrvTarget    string `json:"srv-target,omitempty"`    // SRV -> provider-specific
+	// SrvPriority  string `json:"srv-priority,omitempty"`  // SRV -> provider-specific
+	// SrvWeight    string `json:"srv-weight,omitempty"`    // SRV -> provider-specific
+	// NS           string `json:"ns,omitempty"`            // NS -> provider-specific
+	// ForwardTo    string `json:"forward-to,omitempty"`    // FWD
+}
+
+// NewDNSRecord converts an ExternalDNS Endpoint to a Mikrotik DNSRecord
+func NewDNSRecord(endpoint *endpoint.Endpoint) (*DNSRecord, error) {
+	log.Debugf("converting ExternalDNS endpoint to MikrotikDNS: %v", endpoint)
+
+	record := &DNSRecord{Name: endpoint.DNSName}
+	log.Debugf("Name set to: %s", record.Name)
+
+	record.Type = endpoint.RecordType
+	log.Debugf("Type set to: %s", record.Type)
+
+	switch record.Type {
+	case "A", "AAAA":
+		record.Address = endpoint.Targets[0]
+		log.Debugf("Address set to: %s", record.Address)
+	case "CNAME":
+		record.CName = endpoint.Targets[0]
+		log.Debugf("CName set to: %s", record.CName)
+	case "TXT":
+		record.Text = endpoint.Targets[0]
+		log.Debugf("Text set to: %s", record.Text)
+
+	default:
+		return nil, fmt.Errorf("unsupported DNS type: %s", endpoint.RecordType)
+	}
+
+	ttl, err := endpointTTLtoMikrotikTTL(endpoint.RecordTTL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert ExternalDNS endpoint to MikrotikDNS: %v", err)
+	}
+	record.TTL = ttl
+	log.Debugf("TTL set to: %s", record.TTL)
+
+	for _, providerSpecific := range endpoint.ProviderSpecific {
+		switch providerSpecific.Name {
+		case "comment":
+			record.Comment = providerSpecific.Value
+			log.Debugf("Comment set to: %s", record.Comment)
+		case "regexp":
+			record.Regexp = providerSpecific.Value
+			log.Debugf("Regexp set to: %s", record.Regexp)
+		case "match-subdomain":
+			record.MatchSubdomain = providerSpecific.Value
+			log.Debugf("MatchSubdomain set to: %s", record.MatchSubdomain)
+		case "address-list":
+			record.AddressList = providerSpecific.Value
+			log.Debugf("AddressList set to: %s", record.AddressList)
+
+		default:
+			return nil, fmt.Errorf(
+				"unsupported provider specific configuration '%s' for DNS Record of type %s ",
+				providerSpecific.Name,
+				record.Type,
+			)
+		}
+	}
+
+	log.Debugf("Converted ExternalDNS endpoint to MikrotikDNS: %v", record)
+	return record, nil
+}
+
+// toExternalDNSEndpoint converts a Mikrotik DNSRecord to an ExternalDNS Endpoint
+func (r *DNSRecord) toExternalDNSEndpoint() (*endpoint.Endpoint, error) {
+	log.Debugf("converting MikrotikDNS record to ExternalDNS: %v", r)
+
+	if r.Type == "" {
+		log.Debugf("Record type not set. Using default value 'A'")
+		r.Type = "A"
+	}
+
+	ep := endpoint.Endpoint{
+		DNSName:    r.Name,
+		RecordType: r.Type,
+	}
+	log.Debugf("DNSName set to: %s", ep.DNSName)
+	log.Debugf("RecordType set to: %s", ep.RecordType)
+
+	switch ep.RecordType {
+	case "A", "AAAA":
+		ep.Targets = endpoint.NewTargets(r.Address)
+	case "CNAME":
+		ep.Targets = endpoint.NewTargets(r.CName)
+	case "TXT":
+		ep.Targets = endpoint.NewTargets(r.Text)
+
+	default:
+		return nil, fmt.Errorf("unsupported DNS type: %s", ep.RecordType)
+	}
+	log.Debugf("Targets set to: %v", ep.Targets)
+
+	ttl, err := mikrotikTTLtoEndpointTTL(r.TTL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert MikrotikDNS record to ExternalDNS: %v", err)
+	}
+	ep.RecordTTL = ttl
+	log.Debugf("RecordTTL set to: %v", ep.RecordTTL)
+
+	if r.Comment != "" {
+		ep.ProviderSpecific = append(ep.ProviderSpecific, endpoint.ProviderSpecificProperty{
+			Name:  "comment",
+			Value: r.Comment,
+		})
+		log.Debugf("Comment set to: %s", r.Comment)
+	}
+	if r.Regexp != "" {
+		ep.ProviderSpecific = append(ep.ProviderSpecific, endpoint.ProviderSpecificProperty{
+			Name:  "regexp",
+			Value: r.Regexp,
+		})
+		log.Debugf("Regexp set to: %s", r.Regexp)
+	}
+	if r.MatchSubdomain != "" {
+		ep.ProviderSpecific = append(ep.ProviderSpecific, endpoint.ProviderSpecificProperty{
+			Name:  "match-subdomain",
+			Value: r.MatchSubdomain,
+		})
+		log.Debugf("MatchSubdomain set to: %s", r.MatchSubdomain)
+	}
+	if r.AddressList != "" {
+		ep.ProviderSpecific = append(ep.ProviderSpecific, endpoint.ProviderSpecificProperty{
+			Name:  "address-list",
+			Value: r.AddressList,
+		})
+		log.Debugf("AddressList set to: %s", r.AddressList)
+	}
+
+	log.Debugf("Converted MikrotikDNS record to ExternalDNS: %v", ep)
+	return &ep, nil
+}
+
+// MikrotikTTLtoEndpointTTL converts a Mikrotik TTL to an ExternalDNS TTL
+func mikrotikTTLtoEndpointTTL(ttl string) (endpoint.TTL, error) {
+	log.Debugf("Converting Mikrotik TTL to Endpoint TTL: %s", ttl)
+
+	// i think this should realistically never happen. if it does, it's perhaps a bug?
+	// Mikrotik sets TTL by default on all records, so it should always be set
+	if ttl == "" {
+		log.Warnf("Found a Mikrotik Endpoint with no TTL?! Setting TTL to 0")
+		ttl = "0s"
+	}
+
+	duration, err := time.ParseDuration(ttl)
+	if err != nil {
+		return endpoint.TTL(0), fmt.Errorf("failed to parse duration: %v", err)
+	}
+	log.Debugf("Parsed duration: %v", duration)
+
+	log.Debugf("Converted TTL: %v", duration.Seconds())
+	return endpoint.TTL(duration.Seconds()), nil
+}
+
+// EndpointTTLtoMikrotikTTL converts an ExternalDNS TTL to a Mikrotik TTL.
+// If no TTL is configured in the ExternalDNS endpoint, the default TTL is used.
+func endpointTTLtoMikrotikTTL(ttl endpoint.TTL) (string, error) {
+	log.Debugf("Converting Endpoint TTL to Mikrotil TTL: %v", ttl)
+
+	ttlString := "0s"
+
+	if ttl > 0 {
+		log.Debugf("ExternalDNS Endpoint has TTL defined: %v", ttl)
+		ttlString = strconv.FormatInt(int64(ttl), 10) + "s"
+		log.Debugf("Using TTL from endpoint: %s", ttlString)
+	} else {
+		log.Debugf("No TTL configured in the ExternalDNS endpoint. Using TTL: %s", ttlString)
+	}
+
+	duration, err := time.ParseDuration(ttlString)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse TTL: %v", err)
+	}
+
+	log.Debugf("Converted TTL: %v", duration.String())
+	return duration.String(), nil
+}
---
+++ internal/mikrotik/record_test.go
@@ -0,0 +1,142 @@
+package mikrotik
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"sigs.k8s.io/external-dns/endpoint"
+)
+
+func TestRecordConversion(t *testing.T) {
+	// Define test cases
+	tests := []struct {
+		name     string
+		input    endpoint.Endpoint
+		expected DNSRecord
+	}{
+		{
+			name: "Basic A Record Conversion",
+			input: endpoint.Endpoint{
+				DNSName:    "google.com",
+				RecordType: "A",
+				RecordTTL:  300,
+				Targets:    endpoint.Targets{"8.8.8.8"},
+			},
+			expected: DNSRecord{
+				Name:    "google.com",
+				Type:    "A",
+				TTL:     "5m0s",
+				Address: "8.8.8.8",
+			},
+		},
+		{
+			name: "Basic AAAA Record Conversion",
+			input: endpoint.Endpoint{
+				DNSName:    "google.com",
+				RecordType: "AAAA",
+				RecordTTL:  180,
+				Targets:    endpoint.Targets{"2607:f8b0:400a:800::200e"},
+			},
+			expected: DNSRecord{
+				Name:    "google.com",
+				Type:    "AAAA",
+				TTL:     "3m0s",
+				Address: "2607:f8b0:400a:800::200e",
+			},
+		},
+		{
+			name: "Basic CNAME Record Conversion",
+			input: endpoint.Endpoint{
+				DNSName:    "google.com",
+				RecordType: "CNAME",
+				RecordTTL:  120,
+				Targets:    endpoint.Targets{"maps.google.com"},
+			},
+			expected: DNSRecord{
+				Name:  "google.com",
+				Type:  "CNAME",
+				TTL:   "2m0s",
+				CName: "maps.google.com",
+			},
+		},
+		{
+			name: "Basic TXT Record Conversion",
+			input: endpoint.Endpoint{
+				DNSName:    "google.com",
+				RecordType: "TXT",
+				RecordTTL:  100,
+				Targets:    endpoint.Targets{"lorem ipsum dolor sit amet"},
+			},
+			expected: DNSRecord{
+				Name: "google.com",
+				Type: "TXT",
+				TTL:  "1m40s",
+				Text: "lorem ipsum dolor sit amet",
+			},
+		},
+		{
+			name: "Complete Record Conversion",
+			input: endpoint.Endpoint{
+				DNSName:    "google.com",
+				RecordType: "A",
+				RecordTTL:  10,
+				Targets:    endpoint.Targets{"1.1.1.1"},
+				ProviderSpecific: []endpoint.ProviderSpecificProperty{
+					{Name: "comment", Value: "test comment"},
+					{Name: "regexp", Value: "test regexp"},
+					{Name: "match-subdomain", Value: "true"},
+					{Name: "address-list", Value: "test address list"},
+				},
+			},
+			expected: DNSRecord{
+				Name:           "google.com",
+				Type:           "A",
+				TTL:            "10s",
+				Address:        "1.1.1.1",
+				Comment:        "test comment",
+				Regexp:         "test regexp",
+				MatchSubdomain: "true",
+				AddressList:    "test address list",
+			},
+		},
+	}
+
+	// Run test cases
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			record, err := NewDNSRecord(&tt.input)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expected, *record)
+
+			endpoint, err := record.toExternalDNSEndpoint()
+			assert.NoError(t, err)
+			assert.Equal(t, tt.input, *endpoint)
+		})
+	}
+}
+
+func TestNonSupportedRecordType(t *testing.T) {
+	endpoint := endpoint.Endpoint{
+		DNSName:    "name",
+		RecordType: "unsupported",
+		RecordTTL:  0,
+		Targets:    endpoint.Targets{""},
+	}
+
+	record, err := NewDNSRecord(&endpoint)
+	assert.Nil(t, record)
+	assert.Error(t, err)
+}
+
+func TestEmptyRecordType(t *testing.T) {
+	record := &DNSRecord{
+		Name:    "name",
+		Type:    "",
+		TTL:     "10s",
+		Address: "1.1.1.1",
+	}
+
+	endpoint, err := record.toExternalDNSEndpoint()
+	assert.NoError(t, err)
+	assert.Equal(t, endpoint.RecordType, "A")
+}
