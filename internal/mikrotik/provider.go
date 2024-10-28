b748116c2a005f843d544f3a961e66d0f5f3fa8a
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-10-28 18:48:53 +0000

    add plan cleanup function

--- internal/mikrotik/provider.go
+++ internal/mikrotik/provider.go
@@ -64,8 +64,51 @@ func (p *MikrotikProvider) Records(ctx context.Context) ([]*endpoint.Endpoint, e
 	return endpoints, nil
 }
 
+func cleanupChanges(changes *plan.Changes) *plan.Changes {
+	for oldIndex, old := range changes.UpdateOld {
+		for newIndex, new := range changes.UpdateNew {
+			if old.DNSName == new.DNSName && old.Targets[0] == new.Targets[0] && old.RecordTTL == new.RecordTTL {
+				needUpdate := false
+
+				oldComment, _ := old.GetProviderSpecificProperty("comment")
+				newComment, _ := new.GetProviderSpecificProperty("webhook/comment")
+				if oldComment != newComment {
+					needUpdate = true
+				}
+
+				oldMatchSubdomain, _ := old.GetProviderSpecificProperty("match-subdomain")
+				newMatchSubdomain, _ := new.GetProviderSpecificProperty("webhook/match-subdomain")
+				if oldMatchSubdomain != newMatchSubdomain {
+					needUpdate = true
+				}
+
+				oldAddressList, _ := old.GetProviderSpecificProperty("address-list")
+				newAddressList, _ := new.GetProviderSpecificProperty("webhook/address-list")
+				if oldAddressList != newAddressList {
+					needUpdate = true
+				}
+
+				oldRegexp, _ := old.GetProviderSpecificProperty("regexp")
+				newRegexp, _ := new.GetProviderSpecificProperty("webhook/regexp")
+				if oldRegexp != newRegexp {
+					needUpdate = true
+				}
+
+				if !needUpdate {
+					changes.UpdateOld = append(changes.UpdateOld[:oldIndex], changes.UpdateOld[oldIndex+1:]...)
+					changes.UpdateNew = append(changes.UpdateNew[:newIndex], changes.UpdateNew[newIndex+1:]...)
+				}
+			}
+		}
+	}
+
+	return changes
+}
+
 // ApplyChanges applies a given set of changes in the DNS provider.
 func (p *MikrotikProvider) ApplyChanges(ctx context.Context, changes *plan.Changes) error {
+	changes = cleanupChanges(changes)
+
 	for _, endpoint := range append(changes.UpdateOld, changes.Delete...) {
 		if err := p.client.DeleteDNSRecord(endpoint); err != nil {
 			return err
