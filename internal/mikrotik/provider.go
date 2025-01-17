664d75ab90901f2777a6579158afc1151d558261
Mircea-Pavel ANTON <contact@mirceanton.com>
2025-01-17 22:42:47 +0000

    add more logs

--- internal/mikrotik/provider.go
+++ internal/mikrotik/provider.go
@@ -95,58 +95,83 @@ func (p *MikrotikProvider) GetDomainFilter() endpoint.DomainFilterInterface {
 // ================================================================================================
 // UTILS
 // ================================================================================================
-func getProviderSpecific(ep *endpoint.Endpoint, ps string) string {
+// getProviderSpecific retrieves a provider-specific property from the endpoint, looking both values
+// that could come from annotations (i.e. webhook/%s) as well as values from CRD (i.e. %s).
+// If the property is not found, it returns the specified default value.
+func getProviderSpecific(ep *endpoint.Endpoint, ps string, defaultValue string) string {
 	value, valueExists := ep.GetProviderSpecificProperty(ps)
-	if !valueExists {
-		value, _ = ep.GetProviderSpecificProperty(fmt.Sprintf("webhook/%s", ps))
+	if valueExists {
+		log.Debugf("Found provider-specific property '%s' with value: %s", ps, value)
+		return value
 	}
-	return value
+
+	log.Debugf("Provider-specific property '%s' not found, checking for webhook/%s", ps, ps)
+	value, valueExists = ep.GetProviderSpecificProperty(fmt.Sprintf("webhook/%s", ps))
+	if valueExists {
+		log.Debugf("Found provider-specific property 'webhook/%s' with value: %s", ps, value)
+		return value
+	}
+
+	log.Debugf("Property '%s' not found, returning default value: %s", ps, defaultValue)
+	return defaultValue
 }
 
+// compareEndpoints compares two endpoints to determine if they are identical, keeping in mind empty/default states.
 func isEndpointMatching(a *endpoint.Endpoint, b *endpoint.Endpoint) bool {
-	if a.DNSName != b.DNSName || a.Targets[0] != b.Targets[0] || a.RecordTTL != b.RecordTTL {
+	log.Debugf("Comparing endpoint a: %v", a)
+	log.Debugf("Against endpoint b: %v", b)
+
+	if a.DNSName != b.DNSName {
+		log.Debugf("DNSName mismatch: %v != %v", a.DNSName, b.DNSName)
+		return false
+	}
+	if a.Targets[0] != b.Targets[0] {
+		log.Debugf("Targets[0] mismatch: %v != %v", a.Targets[0], b.Targets[0])
 		return false
 	}
 
-	aComment := getProviderSpecific(a, "comment")
-	bComment := getProviderSpecific(b, "comment")
+	if a.RecordTTL != b.RecordTTL {
+		log.Debugf("RecordTTL mismatch: %v != %v", a.RecordTTL, b.RecordTTL)
+		return false
+	}
+
+	aComment := getProviderSpecific(a, "comment", "")
+	bComment := getProviderSpecific(b, "comment", "")
 	if aComment != bComment {
+		log.Debugf("Comment mismatch: %v != %v", aComment, bComment)
 		return false
 	}
 
-	aMatchSubdomain := getProviderSpecific(a, "match-subdomain")
-	if aMatchSubdomain == "" {
-		aMatchSubdomain = "false"
-	}
-	bMatchSubdomain := getProviderSpecific(b, "match-subdomain")
-	if bMatchSubdomain == "" {
-		bMatchSubdomain = "false"
-	}
+	aMatchSubdomain := getProviderSpecific(a, "match-subdomain", "false")
+	bMatchSubdomain := getProviderSpecific(b, "match-subdomain", "false")
 	if aMatchSubdomain != bMatchSubdomain {
+		log.Debugf("MatchSubdomain mismatch: %v != %v", aMatchSubdomain, bMatchSubdomain)
 		return false
 	}
 
-	aDisabled := getProviderSpecific(a, "disabled")
-	if aDisabled == "" {
-		aDisabled = "false"
-	}
-	bDisabled := getProviderSpecific(b, "disabled")
-	if bDisabled == "" {
-		bDisabled = "false"
-	}
+	aDisabled := getProviderSpecific(a, "disabled", "false")
+	bDisabled := getProviderSpecific(b, "disabled", "false")
 	if aDisabled != bDisabled {
+		log.Debugf("Disabled mismatch: %v != %v", aDisabled, bDisabled)
 		return false
 	}
 
-	aAddressList := getProviderSpecific(a, "address-list")
-	bAddressList := getProviderSpecific(b, "address-list")
+	aAddressList := getProviderSpecific(a, "address-list", "")
+	bAddressList := getProviderSpecific(b, "address-list", "")
 	if aAddressList != bAddressList {
+		log.Debugf("AddressList mismatch: %v != %v", aAddressList, bAddressList)
 		return false
 	}
 
-	aRegexp := getProviderSpecific(a, "regexp")
-	bRegexp := getProviderSpecific(b, "regexp")
-	return aRegexp == bRegexp
+	aRegexp := getProviderSpecific(a, "regexp", "")
+	bRegexp := getProviderSpecific(b, "regexp", "")
+	if aRegexp != bRegexp {
+		log.Debugf("Regexp mismatch: %v != %v", aRegexp, bRegexp)
+		return false
+	}
+
+	log.Debugf("Endpoints match successfully.")
+	return true
 }
 
 func contains(haystack []*endpoint.Endpoint, needle *endpoint.Endpoint) bool {
@@ -158,8 +183,12 @@ func contains(haystack []*endpoint.Endpoint, needle *endpoint.Endpoint) bool {
 	return false
 }
 
+// changes processes and filters the changes plan for updates.
+// It adjusts TTL for created endpoints and removes duplicate updates from the plan.
 func (p *MikrotikProvider) changes(changes *plan.Changes) *plan.Changes {
-	// Initialize new plan -> we don't really need to worry about Create or Delete changes.
+	log.Debug("Starting to process changes plan.")
+
+	// Initialize new plan -> we don't really need to worry about Delete changes.
 	// Only updates are sketchy
 	newChanges := &plan.Changes{
 		Create:    []*endpoint.Endpoint{},
@@ -168,36 +197,45 @@ func (p *MikrotikProvider) changes(changes *plan.Changes) *plan.Changes {
 		UpdateNew: []*endpoint.Endpoint{},
 	}
 
-	duplicates := []*endpoint.Endpoint{}
+	log.Debugf("Initial changes - Create: %d, Delete: %d, UpdateOld: %d, UpdateNew: %d", len(changes.Create), len(changes.Delete), len(changes.UpdateOld), len(changes.UpdateNew))
 
+	// Process Create changes
 	for _, create := range changes.Create {
 		if !create.RecordTTL.IsConfigured() {
+			log.Debugf("Setting default TTL for created endpoint: %v", create)
 			create.RecordTTL = endpoint.TTL(p.client.TTL)
 			newChanges.Create = append(newChanges.Create, create)
 		}
 	}
 
+	// Identify duplicates in Update changes
+	duplicates := []*endpoint.Endpoint{}
 	for _, old := range changes.UpdateOld {
 		for _, new := range changes.UpdateNew {
 			if isEndpointMatching(old, new) {
+				log.Debugf("Found duplicate update for endpoint: %v", old)
 				duplicates = append(duplicates, old)
 			}
 		}
 	}
 
+	// Filter out duplicates from UpdateOld
 	for _, old := range changes.UpdateOld {
 		if !contains(duplicates, old) {
+			log.Debugf("Adding non-duplicate UpdateOld endpoint: %v", old)
 			newChanges.UpdateOld = append(newChanges.UpdateOld, old)
 		}
 	}
+
+	// Filter out duplicates from UpdateNew
 	for _, new := range changes.UpdateNew {
 		if !contains(duplicates, new) {
-			if !new.RecordTTL.IsConfigured() {
-				new.RecordTTL = endpoint.TTL(p.client.TTL)
-			}
+			log.Debugf("Adding non-duplicate UpdateNew endpoint: %v", new)
 			newChanges.UpdateNew = append(newChanges.UpdateNew, new)
 		}
 	}
 
+	log.Debugf("Processed changes - Create: %d, Delete: %d, UpdateOld: %d, UpdateNew: %d", len(newChanges.Create), len(newChanges.Delete), len(newChanges.UpdateOld), len(newChanges.UpdateNew))
+	log.Debug("Finished processing changes plan.")
 	return newChanges
 }
