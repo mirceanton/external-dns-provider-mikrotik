b4a3c5aab79a9587c25ee5c54233a36b813e9142
Mircea-Pavel ANTON <contact@mirceanton.com>
2025-03-07 20:44:20 +0200

    try fix

--- .github/configs/commitlint.config.mjs
+++ .github/configs/commitlint.config.mjs
@@ -1,11 +1,12 @@
-// Extending the conventional commit rules
+import {
+	RuleConfigCondition,
+	RuleConfigSeverity,
+	TargetCaseType,
+} from '@commitlint/types';
+
 export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    // Enforce commit message length
-    "header-max-length": [
-      2, // Error level
-      128 // max length
-    ]
+    'header-max-length': [RuleConfigSeverity.Error, 'always', 128],
   },
 };
