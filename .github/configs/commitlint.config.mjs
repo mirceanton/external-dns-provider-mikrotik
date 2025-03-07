cf647dc1c50a899c0a48fb210784c1626c85d039
Mircea-Pavel ANTON <contact@mirceanton.com>
2025-03-07 20:45:18 +0200

    try fix

--- .github/configs/commitlint.config.mjs
+++ .github/configs/commitlint.config.mjs
@@ -7,6 +7,6 @@ import {
 export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    'header-max-length': [RuleConfigSeverity.Error, 'always', 128],
+    'header-max-length': [2, 'always', 128],
   },
 };
