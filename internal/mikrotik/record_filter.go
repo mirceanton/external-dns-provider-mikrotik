e0655e19aa7d501f2daba8b4ac7245cddda814e4
Mircea-Pavel Anton <contact@mirceanton.com>
2025-12-03 01:07:35 +0200

    fix: type filtering error when encoding params

--- internal/mikrotik/record_filter.go
+++ internal/mikrotik/record_filter.go
@@ -8,19 +8,19 @@ type DNSRecordFilter struct {
 	Type string
 }
 
-// toQueryParams converts a DNSRecordFilter to an encoded query string for the RouterOS API.
+// toQueryParams converts a DNSRecordFilter to a query string for the RouterOS API.
+
 func (f DNSRecordFilter) toQueryParams() string {
-	params := url.Values{}
-
-	if f.Name != "" {
-		params.Set("name", f.Name)
-	}
-
 	recordType := f.Type
 	if recordType == "" {
 		recordType = "A,AAAA,CNAME,TXT,MX,SRV,NS"
 	}
-	params.Set("type", recordType)
 
-	return params.Encode()
+	query := "type=" + recordType
+
+	if f.Name != "" {
+		query += "&name=" + url.QueryEscape(f.Name)
+	}
+
+	return query
 }
